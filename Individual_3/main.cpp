08a2a1b34b3fbacad1803c39566efd8b27320acb
ivanignashin <ivanignashin@mail.ru>
2024-12-09 23:52:03 +0300

    final with move

--- Individual_3/main.cpp
+++ Individual_3/main.cpp
@@ -17,6 +17,14 @@
 	FrameRectangle ellipse1_FR = ellipse1->getFrameRectangle();
 	std::cout << ellipse1_FR.getp().getx() - ellipse1_FR.getw() / 2.0 << ' ' << ellipse1_FR.getp().gety() - ellipse1_FR.geth() / 2.0 << ' ';
 	std::cout << ellipse1_FR.getp().getx() + ellipse1_FR.getw() / 2.0 << ' ' << ellipse1_FR.getp().gety() + ellipse1_FR.geth() / 2.0 << '\n';
+	
+
+	ellipse1->move(10, 5);
+	std::cout << ellipse1->getname() << ' ';
+	std::cout << ellipse1->getarea() << ' ';
+	FrameRectangle ellipse1_FR_M = ellipse1->getFrameRectangle();
+	std::cout << ellipse1_FR_M.getp().getx() - ellipse1_FR_M.getw() / 2.0 << ' ' << ellipse1_FR_M.getp().gety() - ellipse1_FR_M.geth() / 2.0 << ' ';
+	std::cout << ellipse1_FR_M.getp().getx() + ellipse1_FR_M.getw() / 2.0 << ' ' << ellipse1_FR_M.getp().gety() + ellipse1_FR_M.geth() / 2.0 << '\n';
 
 
 	Regular* regular1 = new Regular(Point(0.0, 0.0), Point(1.0, 2.0), Point(3.0, 1.0));
